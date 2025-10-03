f4584081eb5e96fc3d42d389a236a12ddc04b678
Prasan RKP <prasan543e@gmail.com>
2025-10-03 11:09:32 +0530

    few question added

--- Closure/intQnA.js
+++ Closure/intQnA.js
@@ -26,29 +26,78 @@
 // console.log(result.normal());
 // console.log(result.arrow());
 
-
 // Q.3) Closure + this Binding
 
-function outer() {
-  this.val = 1;
-  return () => {
-    return this.val;
-  };
-}
-const obj = { val: 42 };
-const fn = outer.call(obj);
-console.log(fn());
+// function outer() {
+//   this.val = 1;
+//   return () => {
+//     return this.val;
+//   };
+// }
+// const obj = { val: 42 };
+// const fn = outer.call(obj);
+// console.log(fn());
 
 // ans - 1
 
 // Q.4) Closure + event Listener
 
-function setup() {
-  let clicks = 0;
-  document.getElementById("para").addEventListener("click", () => {
-    clicks++;
-    console.log("Clicked:", clicks);
-  });
-}
-setup();
+// function setup() {
+//   let clicks = 0;
+//   document.getElementById("para").addEventListener("click", () => {
+//     clicks++;
+//     console.log("Clicked:", clicks);
+//   });
+// }
+// setup();
 
+//
+
+// Q,5) Secret Password
+
+// function secretPassword() {
+
+//   let pass = "abc123";
+
+//   return function(guess){
+//     if(guess === pass){
+//     console.log("Yes it is your Password");
+//     }
+//     else{
+//        console.log("Failed to guess Password");
+//     }
+//   }
+// }
+
+// let my = secretPassword();
+
+// console.log(my("qwes123"));
+// console.log(my("assdf123"));
+// console.log(my("qwfnfn3"));
+// console.log(my("abc123"));
+
+// Q.6) Rate Limiter/Throttle
+
+
+function throttle(fn, limit){
+  let isThrottle = false;
+
+  return function(...args){
+    if(!isThrottle){
+      fn(...args);
+      isThrottle = true;
+      setTimeout(()=> {
+         isThrottle = false;
+      }, limit)
+    }
+  }
+}
+
+
+const expensive = () => { console.log("I am Expensive & called"); };
+
+let myFn = throttle(expensive, 2000);
+
+myFn(); // Will run immediately
+myFn(); // Ignored (within 2 seconds)
+//setTimeout(myFn, 2100); // Will run after 2.1 seconds
